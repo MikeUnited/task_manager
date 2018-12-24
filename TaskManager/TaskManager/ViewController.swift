f5b28de6a97ea28b6d7c68e41f0ce72687080c24
Michael Misganaw <michealmisganaw@gmail.com>
2018-12-24 17:50:52 +0300

    Initial commit

---
+++ TaskManager/TaskManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskManager
+//
+//  Created by Michael Alene on 12/24/18.
+//  Copyright © 2018 Michael Alene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
